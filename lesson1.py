cbfb87d8f87e014b88a3041457b419bc7af0ec36
EddyMwai <mwaikimemia@gmail.com>
2022-05-17 11:44:46 +0300

    lesson2.py about intergers

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+#Python basic
+
+#This is a comment
+
+#Adding two numbers  a and b
+
+
+#This is a 
+#Multi / many lines 
+#Comments
+
+print("My name is Eddy Mwai")
+
+print("I am from Kiambu")
+
+print("I attended Mang'u high school")
+
+print("Bowling is a game enjoy among others")
---
+++ lesson2.py
@@ -0,0 +1,25 @@
+#my age
+
+Age = 18
+
+noOfSibling = 3
+
+dateOfBirth = 13
+yearOfBirth = 2004
+
+#strings
+
+favouriteFood = "Grilled Turkey"
+schoolAttended = "Mang'u High School"
+
+my_first_name = "Eddy"
+my_second_name = "Mwai"
+
+print("I am " + my_first_name)
+print("I am " + my_second_name)
+
+print("I am " + str (Age) + " years old")
+
+
+print("I attended " + schoolAttended)
+print("My favourite food is " + favouriteFood)
